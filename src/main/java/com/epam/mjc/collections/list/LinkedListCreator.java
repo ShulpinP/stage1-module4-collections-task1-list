16e8bb84390ada26cde4882242fb3d7d7a5bc7f5
ShulpinP <pavelshulpin6@gmail.com>
2022-09-18 17:00:40 +0300

    1

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,14 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> outputList = new LinkedList<>();
+        for (Integer integer : sourceList) {
+            if (integer % 2 == 0) {
+                outputList.addLast(integer);
+            } else {
+                outputList.addFirst(integer);
+            }
+        }
+        return outputList;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -1,15 +1,37 @@
 package com.epam.mjc.collections.list;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
     }
 }
+
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        if (value(a) > value(b)) {
+            return 1;
+        } else if (value(a) < value(b)) {
+            return -1;
+        } else {
+            if (Integer.parseInt(a) > Integer.parseInt(b)) {
+                return 1;
+            } else return -1;
+        }
     }
+
+    public int value(String s) {
+        int element = Integer.parseInt(s);
+        return element * element * 5 - 3;
+    }
+
+
 }
+
+
